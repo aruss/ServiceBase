02dc6416c69b5a2d1544ac6bd32e74ab2de05573
aruss <arusslabs@gmail.com>
2016-10-18 21:43:08 +0200

    Added  RetryHttpClientHandler

---
+++ src/ServiceBase/RetryHttpClientHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBase
+{
+    // https://msdn.microsoft.com/de-de/library/dn589788.aspx
+    /* Example 
+     * 
+     * using (var client = new HttpClient(new RetryHttpClientHandler(3)))
+     * {
+     *      // Client will try 3 times to make a POST request until a success code is returned
+     *      await client.PostAsync("http://domain.com/foo", content);
+     * }
+     */
+
+    public class RetryHttpClientHandler : DelegatingHandler
+    {
+        private int _maxRetries;
+
+        public RetryHttpClientHandler(int maxRetries = 3)
+            :this(new HttpClientHandler(), maxRetries)
+        {
+
+        }
+
+        public RetryHttpClientHandler(HttpMessageHandler innerHandler, int maxRetries = 3)
+            : base(innerHandler)
+        {
+            _maxRetries = maxRetries;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < _maxRetries; i++)
+            {
+                var response = await base.SendAsync(request, cancellationToken);
+                if (response.IsSuccessStatusCode)
+                {
+                    return response;
+                }
+            }
+
+            return null;
+        }
+    }
+}
