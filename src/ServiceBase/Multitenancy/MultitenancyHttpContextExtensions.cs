fe9a0add74ebf9c6dcc2b89d65e5aeaf7f4ed0bb
aruss <arusslabs@gmail.com>
2016-12-06 13:39:12 +0100

    Multitenancy draft

---
+++ src/ServiceBase/Multitenancy/MultitenancyHttpContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ServiceBase.Multitenancy
+{
+    public static class MultitenancyHttpContextExtensions
+    {
+        public static string GetTenantId(this HttpContext context)
+        {
+            return context.Request.Headers["x-tenant-id"];
+        }
+    }
+}
