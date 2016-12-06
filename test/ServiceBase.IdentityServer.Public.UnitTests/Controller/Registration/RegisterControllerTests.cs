b5ca8f9408b4f6ce38adc4d13d20bfd07436cb62
aruss <arusslabs@gmail.com>
2016-12-06 13:38:06 +0100

    Added unit tests project for idsrv web site

---
+++ test/ServiceBase.IdentityServer.Public.UnitTests/Controller/Registration/RegisterControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ServiceBase.IdentityServer.Config;
+using ServiceBase.IdentityServer.Crypto;
+using ServiceBase.IdentityServer.Public.UI.Register;
+using ServiceBase.IdentityServer.Services;
+using ServiceBase.Notification.Email;
+using ServiceBase.Xunit;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ServiceBase.IdentityServer.UnitTests.Controller.Registration
+{
+    [Collection("Register")]
+    public class RegisterControllerTests
+    {
+        [Fact]
+        public async Task RegisterNewUser()
+        {
+            // Arrange
+            var options = new Options<ApplicationOptions>(new ApplicationOptions
+            {
+
+            });
+
+            var mockLogger = new Mock<ILogger<RegisterController>>();
+            var mockInteraction = new Mock<IIdentityServerInteractionService>();
+            var mockUserAccountStore = new Mock<IUserAccountStore>();
+            var mockCrypto = new Mock<ICrypto>();
+            mockCrypto.Setup(c => c.GenerateSalt()).Returns("salt");
+            mockCrypto.Setup(c => c.Hash("salt")).Returns("hash");
+
+
+            var mockEmailService = new Mock<IEmailService>();
+            var mockEventService = new Mock<IEventService>();
+
+            var controller = new RegisterController(
+                options,
+                mockLogger.Object,
+                mockInteraction.Object,
+                mockUserAccountStore.Object,
+                mockCrypto.Object,
+                mockEmailService.Object,
+                mockEventService.Object);
+
+            // Act
+            var result = await controller.Index(new RegisterInputModel
+            {
+                Email = "jim@localhost",
+                Password = "password",
+                PasswordConfirm = "password",
+                ReturnUrl = "http://localhost/foo/bar"
+            });
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<RegisterViewModel>(viewResult.ViewData.Model);
+
+            // do stuff
+
+        }
+    }
+}
