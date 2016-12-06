1bcb730fbc8fbba469a0265d4d47ce96161c912f
aruss <arusslabs@gmail.com>
2016-12-06 13:40:10 +0100

    docs

--- src/ServiceBase.IdentityServer/Config/ApplicationOptions.cs
+++ src/ServiceBase.IdentityServer/Config/ApplicationOptions.cs
@@ -2,16 +2,48 @@
 {
     public class ApplicationOptions
     {
-        public bool RequireAccountVerification { get; set; } = true;
+        // Local account options
+
+        /// <summary>
+        /// If enbaled users may login and register with local account by using email and password
+        /// </summary>
+        public bool LocalAccountEnabled { get; set; } = true;
+
+        public int PasswordHashingIterationCount { get; set; } = 0;
+        public int AccountLockoutFailedLoginAttempts { get; set; } = 5;
+        public int AccountLockoutDuration { get; set; } = 600; // 10 minutes
+
+        /// <summary>
+        /// If enabled users have to confirm the registration by clicking in the link in the mail in order to login.
+        /// </summary>
+        public bool RequireLocalAccountVerification { get; set; } = true;
+
+        // External account options
+
+        /// <summary>
+        /// If enabled users have to confirm the registration if the come from third party IDP
+        /// </summary>
+        public bool RequireExternalAccountVerification { get; set; } = false;
+
+        // Common options
+
+        /// <summary>
+        /// If enabled user may delete his own account
+        /// </summary>
+        public bool AllowAccountDeletion { get; set; } = true;
+
+        /// <summary>
+        /// If user has trouble with login IdSrv will show all possible accounts which are
+        /// </summary>
+        public bool DisplayLoginHints { get; set; } = false;
+
         public bool LoginAfterAccountCreation { get; set; } = true;
         public bool LoginAfterAccountConfirmation { get; set; } = true;
         public bool LoginAfterAccountRecovery { get; set; } = true;
-        public bool AllowAccountDeletion { get; set; } = true;
+
         public int VerificationKeyLifetime { get; set; } = 86400; // 24 hours
-        public int AccountLockoutFailedLoginAttempts { get; set; } = 5;
-        public int AccountLockoutDuration { get; set; } = 600; // 10 minutes
-        public int PasswordHashingIterationCount { get; set; } = 0;
+
         public bool MergeAccountsAutomatically { get; set; } = true;
-        public bool DisplayLoginHints { get; set; } = false;
+
     }
 }
