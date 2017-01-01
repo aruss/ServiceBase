2dbb3851d88e0358e58b38e728feead8f4835ec7
aruss <arusslabs@gmail.com>
2017-01-02 00:09:55 +0100

    Added mapper tests

---
+++ test/ServiceBase.IdentityServer.EntityFramework.UnitTests/Mappers/PersistedGrantMappersTests.cs
@@ -0,0 +1,39 @@
+﻿using ServiceBase.IdentityServer.EntityFramework.Entities;
+using ServiceBase.IdentityServer.EntityFramework.Mappers;
+using Xunit;
+
+namespace IdentityServer4.EntityFramework.UnitTests.Mappers
+{
+    public class PersistedGrantMappersTests
+    {
+        [Fact]
+        public void PersistedGrantModelToEntityConfigurationIsValid()
+        {
+            var model = new Models.PersistedGrant();
+
+            // TODO: set references
+
+            var mappedEntity = model.ToEntity();
+            var mappedModel = mappedEntity.ToModel();
+
+            Assert.NotNull(mappedModel);
+            Assert.NotNull(mappedEntity);
+            PersistedGrantMappers.Mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void PersistedGrantEntityToModelConfigurationIsValid()
+        {
+            var model = new PersistedGrant();
+
+            // TODO: set references
+
+            var mappedModel = model.ToModel();
+            var mappedEntity = mappedModel.ToEntity();
+
+            Assert.NotNull(mappedModel);
+            Assert.NotNull(mappedEntity);
+            PersistedGrantMappers.Mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
