af4ea2da5c0897f87205792f2ea90bdac0e006f6
aruss <arusslabs@gmail.com>
2017-01-02 00:29:08 +0100

    Update to 1.1

--- coverage.sh
+++ coverage.sh
@@ -35,19 +35,19 @@ PROJECTS=(\
 "ServiceBase.IdentityServer.Public.IntegrationTests" \
 "ServiceBase.IdentityServer.Public.UnitTests" \
 "ServiceBase.IdentityServer.UnitTests" \
-"ServiceBase.UnitTests") 
+"ServiceBase.UnitTests")
 
 for PROJECT in "${PROJECTS[@]}"
 do
-   : 
+   :
 $OPENCOVER \
   -target:"c:\Program Files\dotnet\dotnet.exe" \
-  -targetargs:"test -f netcoreapp1.0 $DOTNET_TEST_ARGS test/$PROJECT" \
+  -targetargs:"test -f netcoreapp1.1 $DOTNET_TEST_ARGS test/$PROJECT" \
   -mergeoutput \
   -hideskipped:File \
   -output:$COVERAGE_DIR/coverage.xml \
   -oldStyle \
   -filter:"+[ServiceBase*]* -[ServiceBase.*Tests*]*" \
-  -searchdirs:$testdir/bin/$CONFIG/netcoreapp1.0 \
+  -searchdirs:$testdir/bin/$CONFIG/netcoreapp1.1 \
   -register:user
 done
