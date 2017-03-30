2ef6fe7ee6e452dbaec3c1d4a51814eb3a980553
aruss <arusslabs@gmail.com>
2017-03-30 17:34:58 +0200

    AddUserSecrets refactoring

--- src/ServiceBase/Configuration/ConfigurationSetup.cs
+++ src/ServiceBase/Configuration/ConfigurationSetup.cs
@@ -1,25 +1,22 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.IO;
 
 namespace ServiceBase.Configuration
 {
     public static class ConfigurationSetup
     {
-        public static IConfigurationRoot Configure(IHostingEnvironment environment)
+        public static IConfigurationRoot Configure(IHostingEnvironment environment, Action<IConfigurationBuilder> configAction = null)
         {
             var builder = new ConfigurationBuilder()
                .SetBasePath(environment.ContentRootPath)
                .AddJsonFile(Path.Combine("Config", "config.json"), optional: false, reloadOnChange: true)
                .AddJsonFile(Path.Combine("Config", $"config.{environment.EnvironmentName}.json"), optional: true, reloadOnChange: true);
-
-            if (environment.IsDevelopment())
-            {
-                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
-            }
-
+                        
             builder.AddEnvironmentVariables();
+
+            configAction?.Invoke(builder);
 
             return builder.Build();
         }
