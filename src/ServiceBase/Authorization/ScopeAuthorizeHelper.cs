7a92101a7ab7e8962d6f7b1d05c0e3c3cc20db1b
aruss <arusslabs@gmail.com>
2017-06-22 14:49:05 +0200

    no message

--- src/ServiceBase/Authorization/ScopeAuthorizeHelper.cs
+++ src/ServiceBase/Authorization/ScopeAuthorizeHelper.cs
@@ -7,7 +7,9 @@ namespace ServiceBase.Authorization
 {
     public static class ScopeAuthorizeHelper
     {
-        public static IEnumerable<ScopeAuthorizeAttribute> GetAllScopeAuthorizeAttributes<TController>(Assembly assembly = null, bool fromReferenced = false) 
+        public static IEnumerable<ScopeAuthorizeAttribute> 
+            GetAllScopeAuthorizeAttributes<TController>(
+            Assembly assembly = null, bool fromReferenced = false) 
         {
             var ass = assembly ?? Assembly.GetEntryAssembly();
             var ttype = typeof(TController);
@@ -18,7 +20,8 @@ namespace ServiceBase.Authorization
                 {
                     foreach (var methodInfo in typeInfo.GetMethods())
                     {
-                        foreach (var attribute in methodInfo.GetCustomAttributes<ScopeAuthorizeAttribute>(true))
+                        foreach (var attribute in methodInfo
+                            .GetCustomAttributes<ScopeAuthorizeAttribute>(true))
                         {
                             yield return attribute; 
                         }
@@ -28,7 +31,8 @@ namespace ServiceBase.Authorization
 
             if (fromReferenced)
             {
-                foreach (var items in ass.GetReferencedAssemblies().Select(s => GetAllScopeAuthorizeAttributes<TController>(Assembly.Load(s))))
+                foreach (var items in ass.GetReferencedAssemblies()
+                    .Select(s => GetAllScopeAuthorizeAttributes<TController>(Assembly.Load(s))))
                 {
                     foreach (var item in items)
                     {
@@ -38,17 +42,24 @@ namespace ServiceBase.Authorization
             }
         }
 
-        public static void AddScopePolicies<TController>(this AuthorizationOptions options, string issuer)
+        public static void AddScopePolicies<TController>(
+            this AuthorizationOptions options, 
+            string issuer,
+            Assembly assembly = null, 
+            bool fromReferenced = false)
         {
-            foreach (var attr in GetAllScopeAuthorizeAttributes<TController>())
+            foreach (var attr in 
+                GetAllScopeAuthorizeAttributes<TController>(assembly, fromReferenced))
             {
                 options.AddScopePolicy(attr.Scope, issuer);
             }
         }
         
-        public static void AddScopePolicy(this AuthorizationOptions options, string scope, string issuer)
+        public static void AddScopePolicy(
+            this AuthorizationOptions options, string scope, string issuer)
         {
-            options.AddPolicy(scope, policy => policy.Requirements.Add(new HasScopeRequirement(scope, issuer)));
+            options.AddPolicy(scope, 
+                policy => policy.Requirements.Add(new HasScopeRequirement(scope, issuer)));
         }
     }
 }
