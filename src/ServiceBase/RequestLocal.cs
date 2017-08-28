64dbcf0710f53aded47bccfe66d8c435295c4763
aruss <arusslabs@gmail.com>
2017-08-28 16:49:05 +0200

    Request/Runtime store drafts

---
+++ src/ServiceBase/RequestLocal.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
+
+namespace ServiceBase
+{
+    /// <summary>
+    /// Represents a wrapper like <see cref="ThreadLocal{T}" /> but instead
+    /// being limited to threads its scope is the current http request.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The type of the value.
+    /// </typeparam>
+    public class RequestLocal<TValue> : IDisposable
+    {
+        #region Private Static Fields
+
+        /// <summary>
+        /// Saves the internal id of the last requestlocal instance.
+        /// </summary>
+        private static long _id;
+
+        #endregion
+
+        #region Private Fields
+
+        /// <summary>
+        /// Saves the key used for caching in the http context.
+        /// </summary>
+        private string _key;
+
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RequestLocal{T}" />
+        /// class.
+        /// </summary>
+        public RequestLocal(IHttpContextAccessor httpContextAccessor)
+        {
+            this._httpContextAccessor = httpContextAccessor;
+
+            this._key = string.Format(
+                "requestlocal_{0}_-_{1}",
+                typeof(TValue).FullName,
+                Interlocked.Increment(ref RequestLocal<TValue>._id));
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the request scoped value.
+        /// </summary>
+        public TValue Value
+        {
+            get
+            {
+                return (TValue)this._httpContextAccessor
+                    .HttpContext.Items[this._key];
+            }
+
+            set
+            {
+                this._httpContextAccessor
+                    .HttpContext.Items[this._key] = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets a flag indicating whether the value was already created.
+        /// </summary>
+        public bool IsValueCreated
+        {
+            get
+            {
+                return this._httpContextAccessor
+                    .HttpContext.Items.ContainsKey(this._key);
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Releases the current instance and removes the value out of the
+        /// http context.
+        /// </summary>
+        public void Dispose()
+        {
+            this._httpContextAccessor
+                    .HttpContext.Items.Remove(this._key);
+        }
+
+        #endregion
+    }
+}
+
+
---
+++ src/ServiceBase/RuntimeLocal.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading;
+using Microsoft.AspNetCore.Http;
+
+namespace ServiceBase
+{
+    /// <summary>
+    /// Local data wraps <see cref="RequestLocal{T}"/> and
+    /// <see cref="ThreadLocal{T}"/> and makes it easier to use them both.
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    public class RuntimeLocal<TValue>
+    {
+        /// <summary>
+        /// Saves the data if we're in a web runtime.
+        /// </summary>
+        private RequestLocal<TValue> _requestLocal;
+
+        /// <summary>
+        /// Saves the data if we'rnt in a web runtime.
+        /// </summary>
+        private ThreadLocal<TValue> _threadLocal;
+
+        public RuntimeLocal(IHttpContextAccessor httpContextAccessor)
+        {
+            this._requestLocal = new RequestLocal<TValue>(httpContextAccessor);
+        }
+
+        public RuntimeLocal()
+        {
+            this._threadLocal = new ThreadLocal<TValue>();
+        }
+
+        /// <summary>
+        /// Gets or sets the value
+        /// </summary>
+        public TValue Value
+        {
+            get
+            {
+                if (this._requestLocal == null)
+                {
+                    return this._threadLocal.IsValueCreated
+                            ? this._threadLocal.Value : default(TValue);
+                }
+                else
+                {
+                    return this._requestLocal.IsValueCreated
+                           ? this._requestLocal.Value : default(TValue);
+                }
+            }
+            set
+            {
+                if (this._requestLocal == null)
+                {
+                    this._threadLocal.Value = value;
+                }
+                else
+                {
+                    this._requestLocal.Value = value;
+                }
+            }
+        }
+    }
+}
+
+
