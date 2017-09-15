165096ad36a9c91182af6531796c38461e857f20
aruss <arusslabs@gmail.com>
2017-09-15 17:18:39 +0200

    Added serilog extensions

---
+++ src/ServiceBase/Logging/IServiceCollectionLoggingExtensions.cs
@@ -0,0 +1,53 @@
+﻿namespace Microsoft.Extensions.Logging
+{
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Serilog;
+
+    public static class IServiceCollectionLoggingExtensions
+    {
+        /// <summary>
+        /// Adds Serilog logging services
+        /// </summary>
+        /// <param name="services">Instance of
+        /// <see cref="IServiceCollection"/></param>
+        /// <param name="config">Instance of <see cref="IConfiguration"/>
+        /// </param>
+        /// <returns>Instance of
+        /// <see cref="IServiceCollection"/> provided via
+        /// <paramref name="services"/> argument</returns>
+        public static IServiceCollection AddSerilog(
+            this IServiceCollection services,
+            IConfiguration config)
+        {
+            return services.AddLogging(builder =>
+            {
+                builder.AddSerilog(config);
+            });
+        }
+
+        /// <summary>
+        /// Adds Serilog logger
+        /// </summary>
+        /// <param name="loggingBuilder">Instance of
+        /// <see cref="ILoggingBuilder"/></param>
+        /// <param name="config">Instance of <see cref="IConfiguration"/>
+        /// </param>
+        /// <returns>Instance of
+        /// <see cref="ILoggingBuilder"/> provided via
+        /// <paramref name="loggingBuilder"/> argument</returns>
+        public static ILoggingBuilder AddSerilog(
+            this ILoggingBuilder loggingBuilder,
+            IConfiguration config)
+        {
+            var serilogOptions = config.GetSection("Serilog");
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.ConfigurationSection(serilogOptions)
+                .CreateLogger();
+
+            return loggingBuilder.AddSerilog(logger, true);
+        }
+    }
+}
--- src/ServiceBase/Logging/LogSerializer.cs
+++ src/ServiceBase/Logging/LogSerializer.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
 namespace ServiceBase.Logging
 {
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
     /// <summary>
     /// Helper to JSON serialize object data for logging.
     /// </summary>
