339d936adc7f0c3bbc7ef2297cf20f5ee9405346
aruss <arusslabs@gmail.com>
2017-09-27 15:00:12 +0200

    Initial commit for ELK stack

---
+++ src/ServiceBase/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿
+namespace ServiceBase.Extensions
+{
+    using System.Linq;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Replaces registerd <typeparamref name="TService"/>
+        /// </summary>
+        /// <typeparam name="TService"></typeparam>
+        /// <typeparam name="TImplementation"></typeparam>
+        /// <param name="services"></param>
+        /// <param name="lifetime"></param>
+        /// <returns></returns>
+        public static IServiceCollection Replace<TService, TImplementation>(
+             this IServiceCollection services,
+             ServiceLifetime lifetime)
+             where TService : class
+             where TImplementation : class, TService
+        {
+            ServiceDescriptor descriptorToRemove = services
+                .FirstOrDefault(d => d.ServiceType == typeof(TService));
+
+            services.Remove(descriptorToRemove);
+
+            ServiceDescriptor descriptorToAdd = new ServiceDescriptor(
+                typeof(TService),
+                typeof(TImplementation),
+                lifetime
+            );
+
+            services.Add(descriptorToAdd);
+            
+            return services;
+        }
+    }
+}
