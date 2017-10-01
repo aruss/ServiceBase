63dad8ff09f38e75107c0338086144b7acd6c02c
aruss <arusslabs@gmail.com>
2017-10-01 21:14:29 +0200

    Added TryAdd IServiceCollection extension

--- src/ServiceBase/Extensions/IServiceCollectionExtensions.cs
+++ src/ServiceBase/Extensions/IServiceCollectionExtensions.cs
@@ -24,7 +24,10 @@ namespace ServiceBase.Extensions
             ServiceDescriptor descriptorToRemove = services
                 .FirstOrDefault(d => d.ServiceType == typeof(TService));
 
-            services.Remove(descriptorToRemove);
+            if (descriptorToRemove != null)
+            {
+                services.Remove(descriptorToRemove);
+            }
 
             ServiceDescriptor descriptorToAdd = new ServiceDescriptor(
                 typeof(TService),
@@ -48,7 +51,7 @@ namespace ServiceBase.Extensions
         /// <returns></returns>
         public static IServiceCollection Replace<TService, TImplementation>(
              this IServiceCollection services,
-             Func<IServiceProvider, object> implementationFactory, 
+             Func<IServiceProvider, object> implementationFactory,
              ServiceLifetime lifetime)
              where TService : class
              where TImplementation : class, TService
@@ -56,7 +59,10 @@ namespace ServiceBase.Extensions
             ServiceDescriptor descriptorToRemove = services
                 .FirstOrDefault(d => d.ServiceType == typeof(TService));
 
-            services.Remove(descriptorToRemove);
+            if (descriptorToRemove != null)
+            {
+                services.Remove(descriptorToRemove);
+            }
 
             ServiceDescriptor descriptorToAdd = new ServiceDescriptor(
                 typeof(TService),
@@ -68,5 +74,39 @@ namespace ServiceBase.Extensions
 
             return services;
         }
+
+        public static IServiceCollection TryAdd<TService, TImplementation>(
+           this IServiceCollection services,
+           ServiceLifetime lifetime)
+           where TService : class
+           where TImplementation : class, TService
+        {
+            ServiceDescriptor descriptorToRemove = services
+                .FirstOrDefault(d => d.ServiceType == typeof(TService));
+
+            if (descriptorToRemove == null)
+            {
+                ServiceDescriptor descriptorToAdd = new ServiceDescriptor(
+                    typeof(TService),
+                    typeof(TImplementation),
+                    lifetime
+                );
+
+                services.Add(descriptorToAdd);
+            }
+
+            return services;
+        }
+
+        public static IServiceCollection TryAddSingleton
+            <TService, TImplementation>(
+          this IServiceCollection services)
+          where TService : class
+          where TImplementation : class, TService
+        {
+            return services.TryAdd<TService, TImplementation>(
+                ServiceLifetime.Singleton);
+        }
+
     }
 }
