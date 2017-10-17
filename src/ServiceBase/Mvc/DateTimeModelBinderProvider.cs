9bc2c37dcd3de60116bdd1a98fa086990d5d253a
aruss <arusslabs@gmail.com>
2017-10-17 18:31:15 +0200

    Added DateTimeModelBilder

---
+++ src/ServiceBase/Mvc/DateTimeModelBinderProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace ServiceBase.Mvc
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc.ModelBinding;
+    using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+
+    public class DateTimeModelBinderProvider : IModelBinderProvider
+    {
+        public IModelBinder GetBinder(ModelBinderProviderContext context)
+        {
+            if (context.Metadata.ModelType != typeof(DateTime) &&
+                context.Metadata.ModelType != typeof(DateTime?))
+            {
+                return null;
+            }
+
+            return new BinderTypeModelBinder(typeof(DateTimeModelBinder));
+        }
+    }
+}
