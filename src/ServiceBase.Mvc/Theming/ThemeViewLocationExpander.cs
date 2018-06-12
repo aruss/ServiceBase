aa4c84413fe1546e1699732df80f1392a7a42d58
aruss <arusslabs@gmail.com>
2018-06-13 00:25:09 +0200

    Cleanup

--- src/ServiceBase.Mvc/Theming/ThemeViewLocationExpander.cs
+++ src/ServiceBase.Mvc/Theming/ThemeViewLocationExpander.cs
@@ -2,6 +2,9 @@
 {
     using System.Collections.Generic;
     using Microsoft.AspNetCore.Mvc.Razor;
+    using ServiceBase.Extensions;
+    using System.IO;
+    using System;
 
     /// <summary>
     /// Specifies the contracts for a view location expander that is used by 
@@ -12,6 +15,8 @@
     public class ThemeViewLocationExpander : IViewLocationExpander
     {
         private readonly IThemeInfoProvider _themeInfoProvider;
+        private readonly string _template1;
+        private readonly string _template2;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ThemeViewLocationExpander"/> class.
@@ -19,36 +24,61 @@
         /// <param name="basePath"></param>
         /// <param name="themeInfoProvider"></param>
         public ThemeViewLocationExpander(
-            IThemeInfoProvider themeInfoProvider)
+            IThemeInfoProvider themeInfoProvider,
+            string basePath)
         {
             this._themeInfoProvider = themeInfoProvider;
+
+            if (Path.IsPathRooted(basePath))
+            {
+                throw new NotImplementedException(
+                  "Support for absolute pathes is not yet implemeted");
+
+                // this._template1 = Path.GetFullPath(Path.Combine(
+                //     basePath, "{0}", "Views", "{{1}}", "{{0}}.cshtml"));
+                // 
+                // this._template2 = Path.GetFullPath(Path.Combine(
+                //     basePath, "{0}", "Views", "Shared", "{{0}}.cshtml"));
+            }
+            else
+            {
+                basePath = basePath
+                    .Replace("./", "~/")
+                    .RemoveTrailingSlash();
+
+                this._template1 =
+                    $"{basePath}/{{0}}/Views/{{{{1}}}}/{{{{0}}}}.cshtml";
+
+                this._template2 =
+                    $"{basePath}/{{0}}/Views/Shared/{{{{0}}}}.cshtml";
+            }
         }
 
         public IEnumerable<string> ExpandViewLocations(
             ViewLocationExpanderContext context,
             IEnumerable<string> viewLocations)
         {
-            string requestTheme = context.Values["requestTheme"];
-            string defaultTheme = context.Values["defaultTheme"];
+            string requestTheme = context.Values["RequestTheme"];
+            string defaultTheme = context.Values["DefaultTheme"];
 
-            yield return $"~/Plugins/{requestTheme}/Views/{{1}}/{{0}}.cshtml";
-            yield return $"~/Plugins/{requestTheme}/Views/Shared/{{0}}.cshtml";
+            yield return String.Format(this._template1, requestTheme);
+            yield return String.Format(this._template2, requestTheme);
 
             if (requestTheme != defaultTheme)
             {
-                yield return $"~/Plugins/{defaultTheme}/Views/{{1}}/{{0}}.cshtml";
-                yield return $"~/Plugins/{defaultTheme}/Views/Shared/{{1}}/{{0}}.cshtml";
+                yield return String.Format(this._template1, defaultTheme);
+                yield return String.Format(this._template2, defaultTheme);
             }
         }
 
         public void PopulateValues(ViewLocationExpanderContext context)
-        {   
+        {
             ThemeInfoResult result = this._themeInfoProvider
                 .GetThemeInfoResultAsync()
                 .Result;
 
-            context.Values["requestTheme"] = result.RequestTheme;
-            context.Values["defaultTheme"] = result.DefaultTheme;
+            context.Values["RequestTheme"] = result.RequestTheme;
+            context.Values["DefaultTheme"] = result.DefaultTheme;
         }
     }
 }
