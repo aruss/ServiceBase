ce5373ff7481d3464a87976a92ef28b51e028286
aruss <arusslabs@gmail.com>
2018-06-14 17:03:48 +0200

    Fixed namespaces

--- src/ServiceBase/DependencyInjection/IServiceFactory.cs
+++ src/ServiceBase/DependencyInjection/IServiceFactory.cs
@@ -1,7 +1,7 @@
 // Copyright (c) Russlan Akiev. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
-namespace IdentityBase.DependencyInjection
+namespace ServiceBase.DependencyInjection
 {
     public interface IServiceFactory<TService> where TService : class
     {
