005db834139aeba970af2f0cebf21aa07fec8965
aruss <arusslabs@gmail.com>
2019-08-17 12:12:00 +0200

    Added license comment

--- src/ServiceBase/Localization/RequestLocalizationPipeline.cs
+++ src/ServiceBase/Localization/RequestLocalizationPipeline.cs
@@ -1,4 +1,7 @@
-﻿namespace ServiceBase.Localization
+﻿// Copyright (c) Russlan Akiev. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace ServiceBase.Localization
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.Extensions.Options;
