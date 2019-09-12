c621487ca91ba5c1610b40c9b8bbfbb58e97c7cb
aruss <arusslabs@gmail.com>
2019-09-12 22:23:29 +0200

    Added frontend options handling

---
+++ src/ServiceBase.Mvc/Plugins/IFrontendOptions.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Russlan Akiev. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace ServiceBase.Plugins
+{
+    public interface IFrontendOptions
+    {
+        // IDictionary<string, object> ToDictionary();
+    }
+}
