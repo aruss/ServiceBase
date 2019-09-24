cb000f9b775f96c97da5e56dcdf8286f7a947768
aruss <arusslabs@gmail.com>
2019-09-24 12:48:38 +0200

    Added dynamic plugin loading for frontend

--- src/ServiceBase.Mvc/Plugins/IFrontendOptionsServiceExtensions.cs
+++ src/ServiceBase.Mvc/Plugins/IFrontendOptionsServiceExtensions.cs
@@ -4,6 +4,7 @@
 namespace ServiceBase.Plugins
 {
     using System;
+    using System.Linq; 
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
@@ -18,7 +19,10 @@ namespace ServiceBase.Plugins
         /// </summary>
         /// <returns>Frontend options.</returns>
         public static async Task<IDictionary<string, IDictionary<string, object>>>
-            GetFrontendOptionsAsDictionaryAsync(this IEnumerable<IFrontendOptionsService> services)
+            GetFrontendOptionsAsDictionaryAsync(
+                this IEnumerable<IFrontendOptionsService> services,
+                string[] whiteList
+            )
         {
             var result = new Dictionary<string, IDictionary<string, object>>();
 
@@ -28,6 +32,11 @@ namespace ServiceBase.Plugins
 
                 Type type = options.GetType();
                 string name = type.Name.Replace("FrontendOptions", string.Empty);
+
+                if (!whiteList.Any(c => c.Equals(name, StringComparison.InvariantCultureIgnoreCase))) {
+                    continue; 
+                }
+
                 var valueDict = new Dictionary<string, object>();
 
                 foreach (var propertyInfo in type.GetProperties())
