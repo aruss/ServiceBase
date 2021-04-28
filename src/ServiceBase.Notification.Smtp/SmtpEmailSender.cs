5f09acc05dc51cf1d3f3860afaedd821b7b3544e
aruss <arusslabs@gmail.com>
2021-04-28 22:31:17 +0200

    fix(ServiceBase.Notification.Smtp): Fixed SMTP client, removed MailKit

--- src/ServiceBase.Notification.Smtp/SmtpEmailSender.cs
+++ src/ServiceBase.Notification.Smtp/SmtpEmailSender.cs
@@ -1,87 +1,102 @@
-﻿namespace ServiceBase.Notification.Smtp
+﻿// Copyright (c) Russlan Akiev. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+namespace ServiceBase.Notification.Smtp
 {
-    using System;
+    using System.Net;
+    using System.Net.Mail;
+    using System.Net.Mime;
+    using System.Text;
     using System.Threading.Tasks;
-    using MailKit.Net.Smtp;
     using Microsoft.Extensions.Logging;
-    using MimeKit;
-    using MimeKit.Text;
     using ServiceBase.Notification.Email;
 
     public class SmtpEmailSender : IEmailSender
     {
-        private readonly SmtpOptions options;
-        private readonly ILogger<SmtpEmailSender> logger;
+        private readonly SmtpOptions _options;
+        private readonly ILogger<SmtpEmailSender> _logger;
 
         public SmtpEmailSender(
             SmtpOptions options,
             ILogger<SmtpEmailSender> logger)
         {
-            this.logger = logger;
-            this.options = options;
+            this._logger = logger;
+            this._options = options;
         }
 
+        /// <summary>
+        /// Sends email via <see cref="SmtpClient"/>
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public async Task SendEmailAsync(EmailMessage message)
         {
-            MimeMessage mimeMsg = new MimeMessage();
+            var hasText = !string.IsNullOrWhiteSpace(message.Text);
+            var hasHtml = !string.IsNullOrWhiteSpace(message.Html);
 
-            mimeMsg.From.Add(new MailboxAddress(
-                 String.IsNullOrWhiteSpace(message.EmailFrom) ?
-                 this.options.EmailFrom :
-                 message.EmailFrom));
-
-            mimeMsg.To.Add(new MailboxAddress(message.EmailTo));
-            mimeMsg.Subject = message.Subject;
-
-            if (!String.IsNullOrWhiteSpace(message.Html))
+            // validate message
+            if ((!hasText && !hasHtml) ||
+                string.IsNullOrWhiteSpace(message.Subject) ||
+                string.IsNullOrWhiteSpace(message.EmailFrom) ||
+                string.IsNullOrWhiteSpace(message.EmailTo))
             {
-                mimeMsg.Body = new TextPart(TextFormat.Html)
-                {
-                    Text = message.Html
-                };
-            }
-            else if (!String.IsNullOrWhiteSpace(message.Text))
-            {
-                mimeMsg.Body = new TextPart(TextFormat.Text)
-                {
-                    Text = message.Text
-                };
+                this._logger.LogError("Invalid EmailMessage");
+                return;
             }
 
-            using (var client = new SmtpClient())
+            using (var client = new SmtpClient()
             {
-#if DEBUG
-                // For demo-purposes, accept all SSL certificates (in case
-                // the server supports STARTTLS)
-                client.ServerCertificateValidationCallback =
-                    (s, c, h, e) => true;
-#endif
-
-                client.Connect(
-                    this.options.Host,
-                    this.options.Port,
-                    this.options.UseSsl
-                );
-
-                // Note: since we don't have an OAuth2 token, disable
-                // the XOAUTH2 authentication mechanism.
-                client.AuthenticationMechanisms.Remove("XOAUTH2");
-
-                // Note: only needed if the SMTP server requires authentication
-                if (!String.IsNullOrWhiteSpace(this.options.UserName) &&
-                    !String.IsNullOrWhiteSpace(this.options.Password))
+                Host = this._options.Host,
+                Port = this._options.Port,
+                UseDefaultCredentials = false,
+                EnableSsl = this._options.UseSsl,
+                Credentials = new NetworkCredential(
+                this._options.UserName,
+                this._options.Password
+            )
+            })
+            {
+                using (var mail = new MailMessage())
                 {
-                    client.Authenticate(
-                        this.options.UserName,
-                        this.options.Password
-                    );
+                    mail.From = new MailAddress(message.EmailFrom);
+                    mail.Subject = message.Subject;
+                    mail.BodyEncoding = Encoding.UTF8;
+                    mail.SubjectEncoding = Encoding.UTF8;
+                    mail.To.Add(new MailAddress(message.EmailTo));
+
+                    // has only text 
+                    if (hasText && !hasHtml)
+                    {
+                        mail.Body = message.Text;
+                    }
+                    // has only html
+                    else if (hasHtml && !hasText)
+                    {
+                        mail.Body = message.Html;
+                        mail.IsBodyHtml = true;
+                    }
+                    // has both
+                    else
+                    {
+                        mail.Body = message.Text;
+
+                        AlternateView htmlView = AlternateView
+                          .CreateAlternateViewFromString(message.Html);
+
+                        htmlView.ContentType = new ContentType("text/html");
+                        mail.AlternateViews.Add(htmlView);
+                    }
+
+                    try
+                    {
+                        client.Send(mail);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        this._logger.LogError(ex);
+                    }
                 }
-
-                client.Send(mimeMsg);
-                client.Disconnect(true);
             }
-
-            await Task.CompletedTask; 
         }
     }
 }
