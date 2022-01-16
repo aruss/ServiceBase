032f6b23242657502f8b3c6e8931ca4572453e25
aruss <arusslabs@gmail.com>
2022-01-16 21:33:22 +0100

    ...

--- updeps.sh
+++
@@ -1,7 +0,0 @@
-./tools/updeps.exe ./src/ServiceBase/ServiceBase.csproj ./src/ServiceBase/ServiceBase.nuspec
-./tools/updeps.exe ./src/ServiceBase.Events.RabbitMQ/ServiceBase.Events.RabbitMQ.csproj ./src/ServiceBase.Events.RabbitMQ/ServiceBase.Events.RabbitMQ.nuspec
-./tools/updeps.exe ./src/ServiceBase.Mvc/ServiceBase.Mvc.csproj ./src/ServiceBase.Mvc/ServiceBase.Mvc.nuspec
-./tools/updeps.exe ./src/ServiceBase.Notification.Plivo/ServiceBase.Notification.Plivo.csproj ./src/ServiceBase.Notification.Plivo/ServiceBase.Notification.Plivo.nuspec
-./tools/updeps.exe ./src/ServiceBase.Notification.SendGrid/ServiceBase.Notification.SendGrid.csproj ./src/ServiceBase.Notification.SendGrid/ServiceBase.Notification.SendGrid.nuspec
-./tools/updeps.exe ./src/ServiceBase.Notification.Smtp/ServiceBase.Notification.Smtp.csproj ./src/ServiceBase.Notification.Smtp/ServiceBase.Notification.Smtp.nuspec
-./tools/updeps.exe ./src/ServiceBase.Notification.Twilio/ServiceBase.Notification.Twilio.csproj ./src/ServiceBase.Notification.Twilio/ServiceBase.Notification.Twilio.nuspec
