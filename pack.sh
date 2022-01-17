86dd82847b0a017a387693ffb1e7251152eb05a9
aruss <arusslabs@gmail.com>
2022-01-17 02:03:31 +0100

    cleaned up nuget configuration

---
+++ pack.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+set -o allexport; source .env; set +o allexport
+
+# Set version
+VERSION=5.0.0
+
+# Remove previous builds and artifacts
+find . -iname "bin" -o -iname "obj" -o -iname "artifacts" | xargs rm -rf
+
+# Restore dependencies
+dotnet restore ./ServiceBase.sln
+
+# Build
+dotnet build ./ServiceBase.sln --no-restore --configuration Release /property:Version=$VERSION
+
+# Pack all the nugets
+for PROJECT in "${PROJECTS[@]}"
+do
+mono /usr/local/bin/nuget.exe pack ./src/$PROJECT/$PROJECT.csproj -OutputDirectory ./artifacts/packages -Properties Configuration=Release -version $VERSION
+done
+
+# Push all the nugets
+for PROJECT in "${PROJECTS[@]}"
+do
+mono /usr/local/bin/nuget.exe push ./artifacts/packages/$PROJECT.$VERSION.nupkg -SkipDuplicate -ApiKey $NUGET_API_KEY -Source https://api.nuget.org/v3/index.json
+done
